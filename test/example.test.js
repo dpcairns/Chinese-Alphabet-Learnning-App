5b9e53edd82dd8d817fcd203828869f3c3edf6d8
Nicholas Roberto <njr444@gmail.com>
2020-02-03 08:54:14 -0800

    add alchemy bootstrap

---
+++ test/example.test.js
@@ -0,0 +1,10 @@
+// IMPORT MODULES under test here:
+// import example from '../src/example.js';
+
+const test = QUnit.test;
+
+test('generateRandomAnswer', function(assert) {
+    
+
+    assert.equal(true, false);
+});
