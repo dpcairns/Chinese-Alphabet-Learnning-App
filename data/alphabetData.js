a4f7529926f35e59798a42ac1c6c372aa90d770d
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-02-03 11:24:18 -0800

    added zheng ti data

--- data/alphabetData.js
+++ data/alphabetData.js
@@ -1,7 +1,7 @@
-const shengMu = [{id:'b', name:'b', audio:'b.mp3'},
-{id:'p', name:'p',audio:'p.mp3'},
-{id:'m', name:'m',audio:'m.mp3'},
-{id:'f', name:'f',audio: 'f.mp3'}];
+const shengMu = [{ id: 'b', name: 'b', audio: 'b.mp3' },
+{ id: 'p', name: 'p', audio: 'p.mp3' },
+{ id: 'm', name: 'm', audio: 'm.mp3' },
+{ id: 'f', name: 'f', audio: 'f.mp3' }];
 
 
 
@@ -28,10 +28,10 @@ const shengMu = [{id:'b', name:'b', audio:'b.mp3'},
 
 
 
-const yunMu = [{id:'a', name:'a', audio:'a.mp3'},
-{id:'o', name:'o',audio:'o.mp3'},
-{id:'e', name:'e',audio:'e.mp3'},
-{id:'i', name:'i',audio: 'yi.mp3'}];
+const yunMu = [{ id: 'a', name: 'a', audio: 'a.mp3' },
+{ id: 'o', name: 'o', audio: 'o.mp3' },
+{ id: 'e', name: 'e', audio: 'e.mp3' },
+{ id: 'i', name: 'i', audio: 'yi.mp3' }];
 
 
 
@@ -77,7 +77,22 @@ const yunMu = [{id:'a', name:'a', audio:'a.mp3'},
 
 
 
-const zhengTi = [{id:'zhi', name:'zhi', audio:'zhi.mp3'},
-{id:'chi', name:'chi',audio:'chi.mp3'},
-{id:'shi', name:'shi',audio:'shi.mp3'},
-{id:'ri', name:'ri',audio: 'ri.mp3'}];
+const zhengTi = [{ id: 'zhi', name: 'zhi', audio: 'zhi.mp3' },
+{ id: 'chi', name: 'chi', audio: 'chi.mp3' },
+{ id: 'shi', name: 'shi', audio: 'shi.mp3' },
+{ id: 'ri', name: 'ri', audio: 'ri.mp3' },
+{ id: 'zi', name: 'zi', audio: 'zi.mp3' },
+{ id: 'ci', name: 'ci', audio: 'ci.mp3' },
+{ id: 'si', name: 'si', audio: 'si.mp3' },
+{ id: 'yi', name: 'yi', audio: 'yi.mp3' },
+{ id: 'wu', name: 'wu', audio: 'wu.mp3' },
+{ id: 'yu', name: 'yu', audio: 'yu.mp3' },
+{ id: 'ye', name: 'ye', audio: 'ye.mp3' },
+{ id: 'yue', name: 'yue', audio: 'yue.mp3' },
+{ id: 'yuan', name: 'yuan', audio: 'yuan.mp3' },
+{ id: 'yin', name: 'yin', audio: 'yin.mp3' },
+{ id: 'yun', name: 'yun', audio: 'yun.mp3' },
+{ id: 'ying', name: 'ying', audio: 'ying.mp3' },
+
+
+];
