bbd92ade073deb050c0b169ce943b00da0c54a39
unknown <ziyi.venger@gmai.com>
2020-02-03 07:25:41 -0800

    add data

---
+++ data/alphabetData.js
@@ -0,0 +1,83 @@
+const shengMu = [{id:'b', name:'b', audio:'b.mp3'},
+{id:'p', name:'p',audio:'p.mp3'},
+{id:'m', name:'m',audio:'m.mp3'},
+{id:'f', name:'f',audio: 'f.mp3'}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const yunMu = [{id:'a', name:'a', audio:'a.mp3'},
+{id:'o', name:'o',audio:'o.mp3'},
+{id:'e', name:'e',audio:'e.mp3'},
+{id:'i', name:'i',audio: 'yi.mp3'}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const zhengTi = [{id:'zhi', name:'zhi', audio:'zhi.mp3'},
+{id:'chi', name:'chi',audio:'chi.mp3'},
+{id:'shi', name:'shi',audio:'shi.mp3'},
+{id:'ri', name:'ri',audio: 'ri.mp3'}];
