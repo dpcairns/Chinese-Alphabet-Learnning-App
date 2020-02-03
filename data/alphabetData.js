b07db1784284908a5f9f1f5ad42df41baf6d09d0
Nicholas Roberto <njr444@gmail.com>
2020-02-03 11:29:23 -0800

    add yunmu data section

--- data/alphabetData.js
+++ data/alphabetData.js
@@ -1,7 +1,3 @@
-const shengMu = [{id:'b', name:'b', audio:'b.mp3'},
-{id:'p', name:'p',audio:'p.mp3'},
-{id:'m', name:'m',audio:'m.mp3'},
-{id:'f', name:'f',audio: 'f.mp3'}];
 
 
 
@@ -28,10 +24,32 @@ const shengMu = [{id:'b', name:'b', audio:'b.mp3'},
 
 
 
-const yunMu = [{id:'a', name:'a', audio:'a.mp3'},
-{id:'o', name:'o',audio:'o.mp3'},
-{id:'e', name:'e',audio:'e.mp3'},
-{id:'i', name:'i',audio: 'yi.mp3'}];
+const yunMu = [
+    { id:'a', name:'a', audio:'a.mp3' },
+    { id:'o', name:'o',audio:'o.mp3' },
+    { id:'e', name:'e',audio:'e.mp3' },
+    { id:'i', name:'i',audio: 'yi.mp3' },
+    { id:'u', name:'u',audio: 'u.mp3' },
+    { id:'v', name:'v',audio: 'v.mp3' },
+    { id:'ai', name:'ai',audio: 'ai.mp3' },
+    { id:'ei', name:'ei',audio: 'ei.mp3' },
+    { id:'ui', name:'ui',audio: 'ui.mp3' },
+    { id:'ao', name:'ao',audio: 'ao.mp3' },
+    { id:'ou', name:'ou',audio: 'ou.mp3' },
+    { id:'iu', name:'iu',audio: 'iu.mp3' },
+    { id:'ie', name:'ie',audio: 'ie.mp3' },
+    { id:'ve', name:'ve',audio: 've.mp3' },
+    { id:'er', name:'er',audio: 'er.mp3' },
+    { id:'an', name:'an',audio: 'an.mp3' },
+    { id:'en', name:'en',audio: 'en.mp3' },
+    { id:'in', name:'in',audio: 'in.mp3' },
+    { id:'un', name:'un',audio: 'un.mp3' },
+    { id:'vn', name:'vn',audio: 'vn.mp3' },
+    { id:'ang', name:'ang',audio: 'ang.mp3' },
+    { id:'eng', name:'eng',audio: 'eng.mp3' },
+    { id:'ing', name:'ing',audio: 'ing.mp3' },
+    { id:'ong', name:'ong',audio: 'ong.mp3' }
+];
 
 
 
@@ -76,8 +94,3 @@ const yunMu = [{id:'a', name:'a', audio:'a.mp3'},
 
 
 
-
-const zhengTi = [{id:'zhi', name:'zhi', audio:'zhi.mp3'},
-{id:'chi', name:'chi',audio:'chi.mp3'},
-{id:'shi', name:'shi',audio:'shi.mp3'},
-{id:'ri', name:'ri',audio: 'ri.mp3'}];
