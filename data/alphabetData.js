33787c05407815485b31231ce2c3d6746b967be7
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-03 11:18:03 -0800

    create initial shengMu data

--- data/alphabetData.js
+++ data/alphabetData.js
@@ -1,8 +1,31 @@
-const shengMu = [{id:'b', name:'b', audio:'b.mp3'},
-{id:'p', name:'p',audio:'p.mp3'},
-{id:'m', name:'m',audio:'m.mp3'},
-{id:'f', name:'f',audio: 'f.mp3'}];
+const shengMu = [
+    { id:'b', name:'b', audio:'b.mp3' },
+    { id:'p', name:'p', audio:'p.mp3' },
+    { id:'m', name:'m', audio:'m.mp3' },
+    { id:'f', name:'f', audio: 'f.mp3' },
+    { id:'d', name:'d', audio: 'd.mp3' },
+    { id:'t', name:'t', audio: 't.mp3' },
+    { id:'n', name:'n', audio: 'n.mp3' },
+    { id:'l', name:'l', audio: 'l.mp3' },
+    { id:'g', name:'g', audio: 'g.mp3' },
+    { id:'k', name:'k', audio: 'k.mp3' },
+    { id:'h', name:'h', audio: 'h.mp3' },
+    { id:'j', name:'j', audio: 'j.mp3' },
+    { id:'q', name:'q', audio: 'q.mp3' },
+    { id:'x', name:'x', audio: 'x.mp3' },
+    { id:'zh', name:'zh', audio: 'zh.mp3' },
+    { id:'ch', name:'ch', audio: 'ch.mp3' },
+    { id:'sh', name:'sh', audio: 'sh.mp3' },
+    { id:'r', name:'r', audio: 'r.mp3' },
+    { id:'z', name:'z', audio: 'z.mp3' },
+    { id:'c', name:'c', audio: 'c.mp3' },
+    { id:'s', name:'s', audio: 's.mp3' },
+    { id:'y', name:'y', audio: 'y.mp3' },
+    { id:'w', name:'w', audio: 'w.mp3' },
+];
 
+
+    
 
 
 
