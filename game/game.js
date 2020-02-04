84f83ccc1283d8b92615f953b831920b09ff2073
zhao ziyi <zhaoziy@oregonstate.edu>
2020-02-04 13:42:45 -0800

    nothing

--- game/game.js
+++ game/game.js
@@ -1,53 +1,19 @@
-import { shengMuArray } from '../data/alphabetData.js';
-//import findById from '../utils/findById.js';
-import { findByAudio } from '../utils/findById.js';
+// import { getUser } from '../utils/getuser.js';
+import { alphabetArray } from '../data/alphabetData.js';
 
-// generating a random number by the length of the array
-let index = Math.floor(Math.random() * shengMuArray.length);
-const soundButton = document.getElementById('sound-button');
-// assigning a random sound from the array as correct answer
-const randomShengMuSound = shengMuArray[index].audio;
-const correctAnswer = findByAudio(shengMuArray, randomShengMuSound);
+function renderLink(alphabetArray){
+    const nav = document.getElementById('nav');
+    const link = document.createElement('a');
+    console.log(link);
+    link.classList.add('alphabetArray');
+    nav.appendChild(link);
+    link.textContent = alphabetArray.title;
+    link.href = '../alphabetArray/?id=' + alphabetArray.id;
+    console.log(alphabetArray.id);
 
-
-// console.log(randomShengMuSound);
-
-// assigning the random sound to play back from the button when pushed
-soundButton.addEventListener('click', () => {
-    const audio = document.getElementById('randomShengMu');
-    audio.src = '../assets/shengmu/' + randomShengMuSound;
-    audio.type = 'audio/mp3';
-    audio.play();
-    soundButton.appendChild(audio);
-
-});
-
-// generates random choices for the test question
-export function generateRandomChoices(arr, numOfChoices) {
-    // creates an empty array 
-    const output = [];
-    let currentArray = arr;
-
-    // loop through the array and grab a random choice for each number of choices
-    for (let i = 0; i < numOfChoices; i++) {
-        let choiceIndex = Math.floor(Math.random() * currentArray.length);
-
-        // populate the empty array with .push for each choice needed
-        output.push(currentArray[choiceIndex]);
-
-        // checking that the current array isn't duplicated        
-        currentArray = currentArray.filter(item => {
-            return item !== currentArray[choiceIndex];
-        });
-    }
-    // add the correct answer to the array
-    output.push(correctAnswer);
-    return output;
+    return link;
 }
-
-// removing the correct answer from available answers
-export const filteredArray = shengMuArray.filter(item => item.id !== correctAnswer.id);
-
-
-export const fourChoiceArray = (generateRandomChoices(filteredArray, 3));
-console.log(fourChoiceArray);
+for (let i = 0; i < alphabetArray.length; i++){
+    const objectFromAlphabetArray = alphabetArray[i];
+    renderLink(objectFromAlphabetArray);
+}
--- game/render-quiz.js
+++ game/render-quiz.js
@@ -1,21 +1,65 @@
-import { generateRandomChoices, fourChoiceArray } from './game.js';
-fourChoiceArray.forEach(choice =>{
-    const choiceForm = document.getElementById('choice-form');
-    const choiceContainer = document.createElement('label');
-    choiceForm.appendChild(choiceContainer);
-    const displayAlphabetSpan = document.createElement('span');
-    choiceContainer.appendChild(displayAlphabetSpan);
-    displayAlphabetSpan.textContent = choice.name;
-    const radioChoice = document.createElement('input');
-    choiceContainer.appendChild(radioChoice);
-    console.log(choiceForm);
+import { generateQuestion } from '../utils/generate-quiz-questions.js';
+import { shengMuArray, yunMuArray, zhengTiArray } from '../data/alphabetData.js';
+import { getUser } from '../utils/getuser.js';
+import { saveUser } from '../utils/saveuser.js';
 
-    radioChoice.value = '';
-    radioChoice.type = 'radio';
-    radioChoice.name = choice.name;
+/*after user makes selection, evaluate answer, remove current question from quiz questions. re-run same logic with remaining questions in array.*/
+let quizQuestions = yunMuArray.slice();
+let currentQuestion = generateQuestion(quizQuestions);
+const soundButton = document.getElementById('sound-button');
+const choiceText = document.getElementById('choices');
+const choiceForm = document.getElementById('choice-form');
+
+// bring the user from local storage
+const user = getUser();
+
+soundButton.addEventListener('click', () => {
+    /*change the name on html to something like randomSoundFromData instead of randomShengmu or make into a function that takes the users quiz choice and generates a sound from that assets file that corresponds.*/
+    const audio = document.getElementById('randomSoundFromData');
+    audio.src = '../assets/yunmu/' + currentQuestion.audio;
+    audio.type = 'audio/mp3';
+    audio.play();
+});
+//array of choices with answer inserted at a random index.
+const choices = generateChoices(currentQuestion.wrongAnswers, generateInsertIndex(), currentQuestion.id);
+console.log(choices);
+choices.forEach(item => {
+    const label = document.createElement('label');
+    const answerOption = document.createElement('input');
+    label.textContent = item;
+    answerOption.value = item;
+    answerOption.type = 'radio';
+    answerOption.name = 'answers';
+    choiceText.appendChild(label);
+    choiceText.appendChild(answerOption);
 });
 
-// choiceForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
 
-// });
+
+//returns a randomized index between 0 and 3.
+function generateInsertIndex() {
+    return Math.floor(Math.random() * 4 + 1) - 1;
+}
+//takes in and array, index(from generateInsertIndex), newItem(selected answer) and inserts the selected answer t a random index in the returned array.
+function generateChoices(arr, index, newItem) {
+    return [
+        ...arr.slice(0, index),
+        newItem,
+        ...arr.slice(index)
+    ];
+}
+
+
+choiceForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(choiceForm);
+    const userChoice = formData.get('answers');
+    if (userChoice === currentQuestion.id) {
+        user.correct++;
+    } else {
+        user.incorrect++;
+    }
+    saveUser(user);
+    console.log(user);
+});
+
